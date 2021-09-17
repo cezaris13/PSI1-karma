db3b91741a9d8460ba9834cc8dad768ca35fcf3d
Martinas Mačernius <Martinasmac@gmail.com>
2021-09-17 17:01:20 +0300

    Item

---
+++ Server/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+
+namespace Karma.Server.Models
+{
+    [DataContract]
+    public class Item
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public ItemState State { get; set; }
+
+        [DataMember]
+        public int Id { get; }
+
+        public Item (string givenName, string givenDescription, int givenId)
+        {
+            Name = givenName;
+            Description = givenDescription;
+            Id = givenId;
+            State = ItemState.Ready;
+        }
+    }
+}
