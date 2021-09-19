0bbfedbce0ed76f72a3f90d47a5c07c01ffec10e
ArentasMeinorius <arentas.meinorius@gmail.com>
2021-09-19 19:18:13 +0300

    Martinas PR migrated

---
+++ File/IFileContentProvider .cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Karma.Server.Models;
+
+namespace Karma.Server.File
+{
+    interface IFileContentProvider
+    {
+        Task<List<Item>> ReadFromFile ();
+
+        Task WriteToFile (List<Item> listToWrite);
+    }
+}
