c02cf7f1479bd27bd2b711dabb24173f9b1f61e4
Arentas <arentas.meinorius@gmail.com>
2021-10-05 09:07:18 +0300

    special equipment + groupjoin

---
+++ Migrations/20211005053952_SpecialEquipment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Karma.Migrations
+{
+    public partial class SpecialEquipment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SpecialEquipment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    OwnerId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpecialEquipment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SpecialEquipment_Volunteers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Volunteers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpecialEquipment_OwnerId",
+                table: "SpecialEquipment",
+                column: "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SpecialEquipment");
+        }
+    }
+}
---
+++ Models/SpecialEquipment.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Karma.Models
+{
+    public class SpecialEquipment
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Volunteer Owner { get; set; }
+
+        public SpecialEquipment(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
