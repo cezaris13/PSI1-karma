b09d447ea0e224b7639868b56b96ee288b53eb15
Pijus Petkevičius <32205463+cezaris13@users.noreply.github.com>
2021-10-05 10:07:35 +0300

    changed private class in SpecialEquipment.razor to struct in volunteerAndEquipment file (#30)

---
+++ Models/VolunteerAndEquipment.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Karma.Models
+{
+    public struct VolunteerAndEquipment
+    {
+        public string VolunteerName { get; set; }
+        public string VolunteerSurname { get; set; }
+        public IEnumerable<string> EquipmentList { get; set; }
+        public VolunteerAndEquipment(string name, string surname, IEnumerable<string> equipment)
+        {
+            VolunteerName = name;
+            VolunteerSurname = surname;
+            EquipmentList = equipment;
+        }
+    }
+}
