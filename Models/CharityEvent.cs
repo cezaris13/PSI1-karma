49bf7d9d695bcf200f1fa0a0198c9dd637305f92
cezar13 <pijus.cezar13@gmail.com>
2021-10-10 17:04:10 +0300

    Added IComparable implementation

--- Models/CharityEvent.cs
+++ Models/CharityEvent.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 
 namespace Karma.Models
 {
-    public class CharityEvent : ICharityEvent
+    public class CharityEvent : ICharityEvent, IComparable
     {
+
         public Guid Id { get; set; }
 
         public string Name { get; set; }
@@ -22,6 +23,28 @@ namespace Karma.Models
             Description = description;
             State = state;
             Volunteers = new List<Volunteer>();
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+                return 1;
+            CharityEvent tempCharityEvent = obj as CharityEvent;
+            if(tempCharityEvent == null)
+                throw new ArgumentException("Object is not a Charity event");
+            else
+            {
+                int compareResult= Name.CompareTo(tempCharityEvent.Name);
+                if (compareResult!= 0)
+                {
+                    return compareResult;
+                }
+                else
+                {
+                    return Description.CompareTo(tempCharityEvent.Description);
+                }
+            }
+
         }
     }
 }
