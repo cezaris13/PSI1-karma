7254a3fa76c79d79436b69c4a062404bd4e1e364
Arentas <arentas.meinorius@gmail.com>
2021-10-19 21:40:36 +0300

    managerId migration was missing

---
+++ Migrations/20211019183730_managerId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Karma.Migrations
+{
+    public partial class managerId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ManagerId",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ManagerId",
+                table: "Events");
+        }
+    }
+}
