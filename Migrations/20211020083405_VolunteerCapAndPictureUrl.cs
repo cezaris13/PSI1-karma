0a549d9cea50657e8091b8a04e21ce01883ce76b
Martinas Mačernius <Martinasmac@gmail.com>
2021-10-20 11:36:26 +0300

    DB migration

---
+++ Migrations/20211020083405_VolunteerCapAndPictureUrl.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Karma.Migrations
+{
+    public partial class VolunteerCapAndPictureUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxVolunteers",
+                table: "Events",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPath",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxVolunteers",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoPath",
+                table: "Events");
+        }
+    }
+}
