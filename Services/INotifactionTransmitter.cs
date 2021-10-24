eae7cb646338461574b5feae806a0080a0b7e609
Arentas <arentas.meinorius@gmail.com>
2021-10-24 12:21:57 +0300

    more cleanup

--- Services/INotifactionTransmitter.cs
+++ Services/INotifactionTransmitter.cs
@@ -8,8 +8,6 @@ namespace Karma.Services
     public interface INotifactionTransmitter
     {
         void ShowMessage(string title);
-        //delegate void ThresholdReachedEventHandler(object sender, NotificationEventArgs e);
         event NotificationEventHandler NotificationEvent;
-        //delegate void NotificationEventHandler(object sender, NotificationEventArgs e);
     }
 }
