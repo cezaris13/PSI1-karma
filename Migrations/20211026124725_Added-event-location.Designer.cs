bd7f00a6760c072e0017cf224478cd1252742bce
cezar13 <pijus.cezar13@gmail.com>
2021-10-26 15:49:06 +0300

    migration, one comment removed

--- Migrations/20211026124725_Added-event-location.Designer.cs
+++ Migrations/20211026124725_Added-event-location.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Karma.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Karma.Migrations
 {
     [DbContext(typeof(KarmaContext))]
-    partial class KarmaContextModelSnapshot : ModelSnapshot
+    [Migration("20211026124725_Added-event-location")]
+    partial class Addedeventlocation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
