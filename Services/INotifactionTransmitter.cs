1c179eb584eb2f273e24f3d34f804fcc6c44726a
Arentas <arentas.meinorius@gmail.com>
2021-10-27 21:11:59 +0300

    alternative events

--- Services/INotifactionTransmitter.cs
+++ Services/INotifactionTransmitter.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using static Karma.Services.NotificationTransmitter;
 
 namespace Karma.Services
@@ -8,6 +9,7 @@ namespace Karma.Services
     public interface INotifactionTransmitter
     {
         void ShowMessage(string title);
-        event NotificationEventHandler NotificationEvent;
+        //event NotificationEventHandler NotificationEvent; //Alternative events
+        event EventHandler<NotificationEventArgs> NotificationEvent;
     }
 }
--- Services/NotificationTransmitter.cs
+++ Services/NotificationTransmitter.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+
 namespace Karma.Services
 {
     public class NotificationTransmitter : INotifactionTransmitter
@@ -11,8 +13,9 @@ namespace Karma.Services
             handler?.Invoke(this, args);
         }
 
-        public event NotificationEventHandler NotificationEvent;
-        public delegate void NotificationEventHandler(object sender, NotificationEventArgs e);
+        //public event NotificationEventHandler NotificationEvent; //Alternative events
+        //public delegate void NotificationEventHandler(object sender, NotificationEventArgs e); //Alternative events
+        public event EventHandler<NotificationEventArgs> NotificationEvent;
 
         public void ShowMessage(string message)
         {
