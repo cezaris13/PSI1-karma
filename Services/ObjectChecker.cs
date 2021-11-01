ada5ad748918379cad5b67dc67a68888a430c582
cezar13 <pijus.cezar13@gmail.com>
2021-11-01 17:38:02 +0200

    fixed discussed problems, when exceptions are thrown, edge cases

---
+++ Services/ObjectChecker.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Reflection;
+
+namespace Karma.Services
+{
+    public static class ObjectChecker
+    {
+        public static bool IsAnyNullOrEmpty(object myObject)
+        {
+            foreach (PropertyInfo pi in myObject.GetType().GetProperties())
+            {
+                if (pi.PropertyType == typeof(string))
+                {
+                    string value = (string) pi.GetValue(myObject);
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
