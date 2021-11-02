f0191b9d64070e7fbbbd361906f1567c594e56ae
cezar13 <pijus.cezar13@gmail.com>
2021-11-02 12:52:39 +0200

    threads, event images

---
+++ Models/EventImages.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Karma.Models
+{
+    public class EventImages
+    {
+        public Guid Id { get; set; }
+
+        public Guid EventId { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public EventImages(Guid id, Guid eventId, string imageUrl)
+        {
+            Id = id;
+            EventId = eventId;
+            ImageUrl = imageUrl;
+        }
+    }
+}
