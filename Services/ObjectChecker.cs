0f9048e3faad8b97b87618ce58b8d98c48ad1347
cezar13 <pijus.cezar13@gmail.com>
2021-11-02 14:37:14 +0200

    fixed comments

--- Services/ObjectChecker.cs
+++ Services/ObjectChecker.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Linq;
 using System.Reflection;
 
 namespace Karma.Services
@@ -9,18 +10,10 @@ namespace Karma.Services
     {
         public static bool IsAnyNullOrEmpty(object myObject)
         {
-            foreach (PropertyInfo pi in myObject.GetType().GetProperties())
-            {
-                if (pi.PropertyType == typeof(string))
-                {
-                    string value = (string) pi.GetValue(myObject);
-                    if (string.IsNullOrEmpty(value))
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
+            return myObject.GetType().GetProperties()
+                .Where(pi => pi.PropertyType == typeof(string))
+                .Select(pi => (string) pi.GetValue(myObject))
+                .Any(value => string.IsNullOrEmpty(value));
         }
     }
 }
