d08e1c7789ca7e5e2e0dbd123dd583f0e4836f7b
cezar13 <pijus.cezar13@gmail.com>
2021-11-02 16:19:20 +0200

    FileContentProvider is back

---
+++ Data/DataModificator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Karma.Models;
+
+namespace Karma.Data
+{
+    //TODO delete after presentation
+    public class DataModificator //: IDataModificator
+    {
+        /*private readonly IFileContentProvider m_fileContentProvider;
+        public DataModificator(
+            IFileContentProvider fileContentprovider)
+        {
+            m_fileContentProvider = fileContentprovider;
+        }
+        public async Task AddCharityEvent(IGenericKarmaItem newCharityEvent)
+        {
+            var charityEventList = (await m_fileContentProvider.ReadFromFileAsync<CharityEvent>()).ToList();
+            charityEventList.Add((CharityEvent)newCharityEvent);
+            await m_fileContentProvider.WriteToFileAsync(charityEventList);
+        }
+        public async Task RemoveCharityEvent(Guid charityEventId)
+        {
+            var charityEventList = (await m_fileContentProvider.ReadFromFileAsync<CharityEvent>()).ToList();
+            var newCharityList = charityEventList.Where(p => p.Id != charityEventId);
+            await m_fileContentProvider.WriteToFileAsync(newCharityList);
+        }
+        public async Task UpdateCharityEvent(IGenericKarmaItem updatedItem)
+        {
+            var charityEventList = (await m_fileContentProvider.ReadFromFileAsync<CharityEvent>()).ToList();
+            int updatedElementId = charityEventList.FindIndex(p => p.Id == updatedItem.Id);
+            if (updatedElementId != -1)
+            {
+                charityEventList[updatedElementId] = (CharityEvent)updatedItem;
+                await m_fileContentProvider.WriteToFileAsync(charityEventList);
+            }
+            else
+            {
+                throw (new IndexOutOfRangeException($"Item with given id :{updatedItem.Id} was not found"));
+            }
+        }*/
+    }
+}
