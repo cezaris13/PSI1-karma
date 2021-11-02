4764cbaf61ba40c211254273705ed3f2e6009e9d
Pijus Petkevičius <32205463+cezaris13@users.noreply.github.com>
2021-11-02 18:58:37 +0200

    FileContentProvider is back (#63)

---
+++ Data/IDataModificator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Karma.Models;
+
+namespace Karma.Data
+{
+    public interface IDataModificator
+    {
+        Task AddCharityEvent(IGenericKarmaItem newCharityEvent);
+
+        Task RemoveCharityEvent(Guid charityEventId);
+
+        Task UpdateCharityEvent(IGenericKarmaItem updatedItem);
+    }
+}
---
+++ Data/IFileContentProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Karma.Data
+{
+    public interface IFileContentProvider
+    {
+        Task<IEnumerable<T>> ReadFromFileAsync<T>();
+
+        Task WriteToFileAsync<T>(IEnumerable<T> listToWrite);
+    }
+}
