f63696b053920e29531a9c86a0a7e4ac6c742e12
Martinas Mačernius <Martinasmac@gmail.com>
2021-11-09 22:11:13 +0200

    Web service
    Added a separate web service project that returns a list of events based on where the user is. In karma project this is used to show events close to users location.

---
+++ APIProject/EventAPI/Controllers/EventController.cs
@@ -0,0 +1,35 @@
+﻿using Karma.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EventAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        [HttpGet]
+        public string GetEvents(string address)
+        {
+            KarmaContext KarmaContext = new KarmaContext();
+            List<CharityEvent> localEvents = new List<CharityEvent>();
+            foreach (var karmaEvent in KarmaContext.Events.ToList())
+            {
+                var regex = new Regex($"(.*){address}(.*)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                var matches = regex.Matches(karmaEvent.Address);
+                if (matches.Count > 0)
+                {
+                    localEvents.Add(karmaEvent);
+                }
+            }
+            return JsonConvert.SerializeObject(localEvents);
+        }
+    }
+}
