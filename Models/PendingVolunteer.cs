d9cf7a96973f04ed695d11195a2061294a95f785
ArentasMeinorius <47771813+ArentasMeinorius@users.noreply.github.com>
2021-11-13 14:37:03 +0200

    Arentas/registration (#72)
    * main functionality for pending user
    
    * ui + cleanup
    
    * code review

---
+++ Models/PendingVolunteer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Karma.Models
+{
+    public class PendingVolunteer
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Guid Id { get; set; }
+
+        public PendingVolunteer(string name, string surname, string phoneNumber, Guid id)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
