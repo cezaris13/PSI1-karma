4f6f04afd3f4ae31bd49169a28bc5dfd3a42b223
Martinas Mačernius <Martinasmac@gmail.com>
2021-11-16 19:19:40 +0200

    Added tabs for all volunteers lists
    Added tabs for all volunteer lists like volunteer list, pending volunteer list, special equipment. Added extra functionality when accepting join requests.

--- Pages/Volunteers.razor.cs
+++ Pages/Volunteers.razor.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using Karma.Models;
+using Karma.Services;
 using MatBlazor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
@@ -21,8 +22,11 @@ namespace Karma.Pages
 
         private KarmaContext m_karmaContext = new();
 
+        public IDBServiceProvider m_DBServiceProvider { get; set; }
+
         string CurrentUserId { get; set; }
 
+        //Active volunteers tab:
         public IEnumerable<IVolunteer> GetVolunteers()
         {
             return m_karmaContext.Volunteers.Include(x => x.Events);
@@ -50,10 +54,54 @@ namespace Karma.Pages
             m_navigationManager.NavigateTo($"volunteer/{id}");
         }
 
+        //Pending volunteers tab:
         protected override void OnInitialized()
         {
             ClaimsPrincipal principal = m_httpContextAccessor.HttpContext.User;
             CurrentUserId = principal.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public IEnumerable<PendingVolunteer> GetPendingVolunteers()
+        {
+            return m_karmaContext.PendingVolunteers;
+        }
+
+        public void AddANewVolunteer(Guid id)
+        {
+            PendingVolunteer pendingVolunteer = m_karmaContext.PendingVolunteers.Where(x => x.Id == id).FirstOrDefault();
+            var volunteer = new Volunteer(pendingVolunteer.Name, pendingVolunteer.Surname, Guid.NewGuid());
+            int result = m_DBServiceProvider.AddToDB(volunteer);
+            if (result == -1)
+                m_notificationTransmitter.ShowMessage("An error occured while adding volunteer to the database", MatToastType.Danger);
+            m_karmaContext.PendingVolunteers.Where(x => x.Id == id).DeleteFromQuery();
+            m_karmaContext.SaveChanges();
+        }
+
+        public void RemovePotentialVolunteer(Guid id)
+        {
+            m_karmaContext.PendingVolunteers.Where(x => x.Id == id).DeleteFromQuery();
+            m_karmaContext.SaveChanges();
+            m_notificationTransmitter.ShowMessage("The request has been deleted", MatToastType.Success);
+        }
+
+        public void NavigateToVolunteerRequest(Guid id)
+        {
+            m_navigationManager.NavigateTo($"pendingvolunteer/{id}");
+        }
+
+        //Equipment tab:
+        private IEnumerable<VolunteerAndEquipment> GetPairsOfVolunteersAndEquipment()
+        {
+            var volunteers = m_karmaContext.Volunteers.ToList();
+            var equipment = m_karmaContext.SpecialEquipment.ToList();
+            return volunteers
+                .GroupJoin(
+                    equipment,
+                    owner => owner,
+                    eq => eq.Owner,
+                    (owner, collection) => new VolunteerAndEquipment(owner.Name, owner.Surname, collection.Select(x => x.Name)))
+                .ToList()
+                .OrderByEquipmentCount();
         }
     }
 }
