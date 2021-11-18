81cd0816055cfd0d480e8c526f046f71aa64bae4
Pijus Petkevičius <32205463+cezaris13@users.noreply.github.com>
2021-11-18 20:35:54 +0200

    Added pagination for event pages (#76)
    * Added pagination for event pages
    
    * Added custom page size field
    
    * renamed 2 variables

--- Pages/ManageUserEvents.razor.cs
+++ Pages/ManageUserEvents.razor.cs
@@ -5,21 +5,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Karma.Models;
 
 namespace Karma.Pages
 {
     public partial class ManageUserEvents
     {
+        private int m_totalPageQuantity;
+        private int m_currentPage = 1;
+        public int perPage;
         public string filterValue = "";
         private string m_currentUserId { get; set; }
 
-        private KarmaContext m_karmaContext = new();
+        public IEnumerable<IGenericKarmaItem> karmaEvents;
 
-        public IEnumerable<IGenericKarmaItem> GetEvents()
-        {
-            return m_karmaContext.Events.Where(p => p.ManagerId == m_currentUserId);
-        }
+        private KarmaContext m_karmaContext = new();
 
         public void NavigateToIndividualEvent(Guid id)
         {
@@ -31,10 +32,36 @@ namespace Karma.Pages
             m_uriHelper.NavigateTo($"editcharityevent/{id}");
         }
 
+        public void SelectedPage(int page)
+        {
+            m_currentPage = page;
+            LoadEvents(page, perPage);
+        }
+
+        private void LoadEvents(int page = 1, int elementsPerPage = 10)
+        {
+            var result = m_karmaContext.Events.Where(p => p.ManagerId == m_currentUserId).ToList();
+            m_totalPageQuantity = Convert.ToInt32(Math.Ceiling(result.Count / (double) elementsPerPage));
+            result.Sort();
+            karmaEvents = result.Skip((page - 1) * elementsPerPage).Take(elementsPerPage);
+        }
+
+        public async Task Refresh(int pageSize)
+        {
+            perPage = pageSize;
+            LoadEvents(elementsPerPage: pageSize);
+            try
+            {
+                await InvokeAsync(StateHasChanged);
+            }
+            catch { }
+        }
+
         protected override void OnInitialized()
         {
             ClaimsPrincipal principal = m_httpContextAccessor.HttpContext.User;
             m_currentUserId = principal.FindFirstValue(ClaimTypes.NameIdentifier);
+            LoadEvents();
         }
     }
 }
