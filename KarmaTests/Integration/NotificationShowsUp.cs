0aefcf5291630262f16a453c9ed7e72aae0b361e
Arentas <arentas.meinorius@gmail.com>
2021-11-28 23:07:34 +0200

    working

---
+++ KarmaTests/Integration/NotificationShowsUp.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Karma.Services;
+using MatBlazor;
+using Moq;
+using NUnit.Framework;
+
+namespace KarmaTests.Integration
+{
+    class NotificationShowsUp
+    {
+        [Test, Combinatorial]
+        public void ShowMessageTest(
+            [Values(null, "", "SingleWord", "Many different words.? !; and stuff")] string message,
+            [Values(MatToastType.Info, MatToastType.Danger, MatToastType.Success)] MatToastType type)
+        {
+            var mockToaster = new Mock<IMatToaster>(MockBehavior.Strict);
+           
+            mockToaster
+                .Setup(x => x.Add(message, type, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<Action>()))
+
+                //.Setup(x => x.Add(message, type, It.IsAny<string>(), It.IsAny<string>(), (Action<MatToastOptions>) It.IsAny<object>()))
+                //.Callback((int interval, bool reset, Action action) => action());
+                //, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<object>()
+                .Returns(new Mock<MatToast>(MockBehavior.Strict).Object);
+            var transmitter = new NotificationTransmitter();
+            var toaster = new NotificationToaster(mockToaster.Object, transmitter);
+
+            transmitter.ShowMessage(message, type);
+
+            mockToaster.VerifyAll();
+        }
+    }
+}
