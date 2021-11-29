bfb7670e3385c0a1db2dcfcccac29fb7e5db3002
Pijus Petkevičius <32205463+cezaris13@users.noreply.github.com>
2021-11-29 21:30:30 +0200

    Pijus/unit tests (#86)
    * unit tests
    
    * removed base unitTest
    
    * more unit tests
    
    * tests and program both work properly now
    
    * forgot to add ShowIndividualEvent
    
    * DbServiceProvider delete test passes!!!

---
+++ KarmaTests/ObjectCheckerTests.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Karma.Models;
+using Karma.Services;
+using NUnit.Framework;
+
+namespace KarmaTests
+{
+    public class ObjectCheckerTests
+    {
+        [Test]
+        public void IsAnyNullOrEmptyTest_TypeContainsNullsAndEmpty_ReturnsTrue()
+        {
+            var obj = new Volunteer("", "", Guid.NewGuid());
+            var sut = new ObjectChecker();
+            bool result = sut.IsAnyNullOrEmpty(obj);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void IsAnyNullOrEmptyTest_TypeDoesNotContainNullsAndEmpty_ReturnsFalse()
+        {
+            var obj = new Volunteer("name", "surname", Guid.NewGuid());
+            var sut = new ObjectChecker();
+            bool result = sut.IsAnyNullOrEmpty(obj);
+
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void IsAnyNullOrEmptyTest_TypeWithoutStrings_ReturnsFalse()
+        {
+            var obj = new TypeWithoutStrings();
+            var sut = new ObjectChecker();
+            bool result = sut.IsAnyNullOrEmpty(obj);
+
+            Assert.AreEqual(false, result);
+        }
+
+        private class TypeWithoutStrings
+        {
+            public int First { get; set; }
+            public Guid Second { get; set; }
+            public bool Third { get; set; }
+        }
+    }
+}
--- Pages/VolunteerRegistration.razor.cs
+++ Pages/VolunteerRegistration.razor.cs
@@ -1,24 +1,34 @@
 using System;
 using Karma.Models;
 using MatBlazor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 
 namespace Karma.Pages
 {
     public partial class VolunteerRegistration
     {
+        [Inject]
+        private IDbContextFactory<KarmaContext> m_contextFactory { get; set; }
+
         private string Name { get; set; } = "";
 
         private string Surname { get; set; } = "";
 
         private string PhoneNumber { get; set; } = "";
 
-        private KarmaContext m_karmaConext = new();
+        private KarmaContext m_karmaContext;
+
+        protected override void OnInitialized()
+        {
+            m_karmaContext = m_contextFactory.CreateDbContext();
+        }
 
         private void CreatePendingVolunteer()
         {
             var volunteer = new PendingVolunteer(Name, Surname, PhoneNumber, Guid.NewGuid());
-            m_karmaConext.PendingVolunteers.Add(volunteer);
-            m_karmaConext.SaveChanges();
+            m_karmaContext.PendingVolunteers.Add(volunteer);
+            m_karmaContext.SaveChanges();
             m_notificationTransmitter.ShowMessage("Registration Succesful", MatToastType.Success);
             Name = "";
             Surname = "";
